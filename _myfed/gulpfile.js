00b5e3b7d8101207569beb348b3cee0dc988223e
myfed <pawel.zietarski@gmail.com>
2016-09-12 14:41:48 +0200

    002

--- _myfed/gulpfile.js
+++ _myfed/gulpfile.js
@@ -16,10 +16,8 @@ var path = {
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-// $ = require('gulp-load-plugins')();
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-clean-css'),
-    // jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
@@ -28,27 +26,19 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    sourcemaps = require('gulp-sourcemaps'),
-    server = lr();
-
-// var sassPaths = [
-//     'node_modules/foundation-sites/scss',
-//     'node_modules/motion-ui/src'
-// ];
+    sourcemaps = require('gulp-sourcemaps');
 
 
 // Styles
 gulp.task('styles', function () {
     return gulp.src(path.src.styles)
+        .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.init())
-        // .pipe(sass({includePaths: sassPaths}).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(concat('style.css'))
         .pipe(gulp.dest(path.dest.styles))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
-        .pipe(livereload(server))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(path.dest.styles))
         .pipe(notify({message: 'Styles task complete'}));
@@ -58,13 +48,10 @@ gulp.task('styles', function () {
 gulp.task('scripts', function () {
     return gulp.src(path.src.scripts)
         .pipe(sourcemaps.init())
-        // .pipe(jshint('.jshintrc'))
-        // .pipe(jshint.reporter('default'))
         .pipe(concat('main.js'))
         .pipe(gulp.dest(path.dest.scripts))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(livereload(server))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(path.dest.scripts))
         .pipe(notify({message: 'Scripts task complete'}));
@@ -74,7 +61,6 @@ gulp.task('scripts', function () {
 gulp.task('images', function () {
     return gulp.src(path.src.images)
         .pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
-        .pipe(livereload(server))
         .pipe(gulp.dest(path.dest.images))
         .pipe(notify({message: 'Images task complete'}));
 });
@@ -93,31 +79,23 @@ gulp.task('default', ['clean'], function () {
 // Watch
 gulp.task('watch', function () {
 
-    // Listen on port 35729
-    server.listen(35729, function (err) {
-        if (err) {
-            return console.log(err)
-        }
-        ;
-
-        // Watch .scss files
-        gulp.watch(path.src.styles, function (event) {
-            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-            gulp.run('styles');
-        });
-
-        // Watch .js files
-        gulp.watch(path.src.scripts, function (event) {
-            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-            gulp.run('scripts');
-        });
-
-        // Watch image files
-        gulp.watch(path.src.images, function (event) {
-            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-            gulp.run('images');
-        });
-
+    // Watch .scss files
+    gulp.watch(path.src.styles, function (event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('styles');
     });
+
+    // Watch .js files
+    gulp.watch(path.src.scripts, function (event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('scripts');
+    });
+
+    // Watch image files
+    gulp.watch(path.src.images, function (event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        gulp.run('images');
+    });
+
 
 });
